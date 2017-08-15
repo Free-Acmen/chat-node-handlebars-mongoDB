47432797202805cb37244b42e59b6d61ea420102
FreeAcmen <lmllsq@126.com>
2017-08-15 16:16:39 +0800

    handlebars模版搭建

--- public/js/Utils.js
+++ public/js/Utils.js
@@ -984,12 +984,13 @@ var Utils = {
             recalc = function() {
                 var clientWidth = docEl.clientWidth;
                 if (!clientWidth) return;
-                //如果屏幕大于750（750是根据我效果图设置的，具体数值参考效果图），就设置clientWidth=750，防止font-size会超过100px
-                if (clientWidth > 750) {
-                    clientWidth = 750
-                }
+
                 //设置根元素font-size大小
-                docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+                if (clientWidth > 750) {
+                    docEl.style.fontSize = '20px';
+                } else {
+                    docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
+                }
             };
         //屏幕大小改变，或者横竖屏切换时，触发函数
         win.addEventListener(resizeEvt, recalc, false);
--- router.js
+++ router.js
@@ -1,6 +1,40 @@
-var Login = require(CON + "login");
+// var Login = require(CON + "login");
 
-exports.router = function(req, res) {
-    var pathname = decodeURI(lib.url.parse(req.url).pathname);
-    lib.httpParam.init(req, res);
+// exports.router = function(req, res) {
+//     var pathname = decodeURI(lib.url.parse(req.url).pathname);
+//     lib.httpParam.init(req, res);
+// }
+
+module.exports = function(chat) {
+    chat.use(function(req, res, next) {
+        if (!res.locals.partials) {
+            res.locals.partials = {};
+        }
+        res.locals.partials.date = new Date();
+        next();
+    });
+
+    chat.use(function(req, res, next) {
+        res.locals.showTests = chat.get('env') !== 'production' && req.query.test === '1';
+        next();
+    });
+
+    chat.get('/', function(req, res) {
+        res.render('chat');
+    });
+
+    //404
+    chat.use(function(req, res) {
+        res.type('text/plain');
+        res.status(404);
+        res.send('404 - Not Found');
+    });
+
+    //500
+    chat.use(function(err, req, res, next) {
+        console.log(err.stack);
+        res.type('text/plain');
+        res.status(500);
+        res.send('500 - Server Error');
+    });
 }
